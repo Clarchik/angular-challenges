40a40c71d3d43acae0bb8cdcd1a1b49961065d33
Clarchik <clarchik.services@gmail.com>
2023-11-11 17:10:12 +0100

    feat(Answer 21): anchor scrolling solution

--- apps/angular/anchor-scrolling/src/main.ts
+++ apps/angular/anchor-scrolling/src/main.ts
@@ -1,8 +1,17 @@
-import { appConfig } from './app/app.config';
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import { appRoutes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err)
-);
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(
+      appRoutes,
+      withInMemoryScrolling({
+        anchorScrolling: 'enabled',
+        scrollPositionRestoration: 'enabled',
+      })
+    ),
+  ],
+}).catch((err) => console.error(err));
